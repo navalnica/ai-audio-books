45ef3d63da31f1b143aae088f86b0fb136f08df3
Maksim <maksim.lyutich@gmail.com>
2024-10-10 21:41:03 +0200

    add voice setting to tts, add sound_generation_astream

--- src/tts.py
+++ src/tts.py
@@ -3,7 +3,7 @@ import typing as t
 
 from dotenv import load_dotenv
 from elevenlabs.client import AsyncElevenLabs, ElevenLabs
-
+from elevenlabs import VoiceSettings
 
 load_dotenv()
 
@@ -26,8 +26,26 @@ def tts(voice_id: str, text: str):
     return combined
 
 
-async def tts_astream(voice_id: str, text: str) -> t.AsyncIterator[bytes]:
-    async_iter = ELEVEN_CLIENT_ASYNC.text_to_speech.convert(voice_id=voice_id, text=text)
+async def tts_astream(voice_id: str, text: str, params: dict) -> t.AsyncIterator[bytes]:
+    async_iter = ELEVEN_CLIENT_ASYNC.text_to_speech.convert(
+        voice_id=voice_id,
+        text=text,
+        voice_settings=VoiceSettings(
+            stability=params['stability'],
+            similarity_boost=params['similarity_boost'],
+            style=params['style']
+        ))
+    async for chunk in async_iter:
+        if chunk:
+            yield chunk
+
+
+async def sound_generation_astream(sound_generation_data: dict) -> t.AsyncIterator[bytes]:
+    async_iter = ELEVEN_CLIENT_ASYNC.text_to_sound_effects.convert(
+        text=sound_generation_data['text'],
+        duration_seconds=sound_generation_data['duration_seconds'],
+        prompt_influence=sound_generation_data['prompt_influence']
+    )
     async for chunk in async_iter:
         if chunk:
             yield chunk
