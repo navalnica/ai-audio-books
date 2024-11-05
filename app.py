5b32cd21cf1ab88d326c84ffd0c477944494582f
Aliaksandr <29257108+navalnica@users.noreply.github.com>
2024-11-05 03:01:10 +0100

    Improve sound effect prompt (#21)
    Improve sound effects generation:
    * upd prompt
    * add fade-in and fade-out
    * make effects quiter
    * make effects at least 1 second long
    * bugfix in effects regex

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ with gr.Blocks(title="Audiobooks Generation") as ui:
     error_output = gr.Textbox(label="Error Message", interactive=False, visible=False)
 
     effects_generation_checkbox = gr.Checkbox(
-        label="Add background effects",
+        label="Add sound effects",
         value=False,
         info="Select if you want to add occasional sound effect to the audiobook",
     )
